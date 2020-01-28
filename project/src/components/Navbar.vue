04c4328387995bf5e2f3420de245364fd0bd8c0a
alekscarlston@yahoo.com <alekscarlston@gmail.com>
2020-01-27 22:12:59 -0700

    Add basic fool navbar

---
+++ project/src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-navbar toggleable="lg" variant="primary">
+    <b-navbar-brand href="#">
+      <img 
+      src="../assets/fool-logo.png" 
+      class="d-inline-block align-top logo" 
+      alt="Kitten"
+      width="157"
+      height="26"
+      />
+    </b-navbar-brand>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="#">Link</b-nav-item>
+        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto"></b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
--- project/src/main.js
+++ project/src/main.js
@@ -6,6 +6,7 @@ import App from '@/App.vue'
 
 import store from '@/store' 
 import router from '@/router'
+import './styles/custom.scss'
 
 Vue.config.productionTip = false
 

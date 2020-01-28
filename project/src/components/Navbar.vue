6a5dd340374f3b052e8cffda2a1f634b5302549c
alekscarlston@yahoo.com <alekscarlston@gmail.com>
2020-01-27 23:04:24 -0700

    Remove stock market news from vue

--- project/src/components/Navbar.vue
+++ project/src/components/Navbar.vue
@@ -12,7 +12,7 @@
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item href="#">Stock News</b-nav-item>
+        
       </b-navbar-nav>
 
       <!-- Right aligned nav items -->
